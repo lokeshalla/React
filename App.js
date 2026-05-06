54927921eadcf8162caf82d4e9090baac6ffe370
Lokesh Alla <allalokeshreddy2002@gmail.com>
2026-05-06 13:19:46 -0500

    JSX and React Components

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
+// JSX - HTML like or XML like syntax
+
+const jsxHeading = (
+  <>
+    <h1 id="jsx-heading">Hello World from JSX</h1>
+    <h2>Sub Heading</h2>
+  </>
+);
+// Function Declaration
+function Heading1() {
+  return (
+    <div>
+      <h1>This is Component 1</h1>
+    </div>
+  );
+}
+// Arrow Function
+const Heading2 = () => {
+  return (
+    <div>
+      <h1>This is Component 2</h1>
+    </div>
+  );
+};
+// You can also write Arrow Function without return statement
+// If there is only one statement
+const Heading3 = () => (
+  <div>
+    <h1>This is Component 3</h1>
+  </div>
 );
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React",
-);
+const Heading4 = () => <h1>This is Component 4</h1>;
+
+const Heading5 = () => {
+  return <h1>This is Component 5</h1>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(
+  <>
+    <Heading1 />
+    <Heading2 />
+    <Heading3 />
+  </>,
+);
